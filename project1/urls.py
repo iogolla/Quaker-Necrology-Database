e84e5cb03eaaabcb34d9bb80d621e0343b4a6782
Andy <apjanco@gmail.com>
2019-07-25 16:57:52 -0400

    changes to views, json view working

--- project1/urls.py
+++ project1/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .import views
 from django.contrib.auth.decorators import login_required
-
+from project1.views import PersonJson
 
 urlpatterns = [
     path("quakers/", views.quaker_index, name='quaker_index'),
     path("<int:pk>/", views.quaker_detail, name='quaker_detail'),
     path("", views.header, name='header'),
     path("quakers/edit/(?P<pk>\d+)", views.edit_quaker, name='edit_quaker'),
+    path('datatable/', PersonJson.as_view(), name='person_json'),
+
 ]
 
 
--- project1/views.py
+++ project1/views.py
@@ -1,54 +1,57 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import * #import all
+from .models import *  # import all
 from .forms import *
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django_datatables_view.base_datatable_view import BaseDatatableView
-from django.utils.html import escape
+from django.utils.html import escape, format_html, mark_safe
+
 
 def header(request):
-	return render(request, 'header.html')
+    return render(request, 'header.html')
 
-#an index view that shows a snippet of information about each quaker
+
+# an index view that shows a snippet of information about each quaker
 def quaker_index(request):
-	quakers_list = Person.objects.all()
-	paginator = Paginator(quakers_list, 20) # Show 25 quakers per page
+    quakers_list = Person.objects.all()
+    paginator = Paginator(quakers_list, 20)  # Show 25 quakers per page
 
-	page = request.GET.get('page')
-	try:
-		quakers = paginator.page(page)
-	except PageNotAnInteger:
-		quakers = paginator.get_page(1)
-	except EmptyPage:
-		quakers = paginator.page(paginator.num_pages)
+    page = request.GET.get('page')
+    try:
+        quakers = paginator.page(page)
+    except PageNotAnInteger:
+        quakers = paginator.get_page(1)
+    except EmptyPage:
+        quakers = paginator.page(paginator.num_pages)
+
+    context = {
+        'quakers': quakers,
+    }
+
+    return render(request, 'quaker_index.html', context)
 
 
-	context = {
-		'quakers' : quakers,
-	}
-
-	return render(request, 'quaker_index.html', context)
-
-#a detail view that shows more information on a particular quaker
+# a detail view that shows more information on a particular quaker
 def quaker_detail(request, pk):
-	quaker = Person.objects.get(pk=pk)
-	form = CommentForm()
-	if request.method == 'POST':
-		form = CommentForm(request.POST)
-		if form.is_valid():
-			comment = Comment(
-				author=form.cleaned_data["author"],
-				body=form.cleaned_data["body"],
-				quaker=quaker
-				)
-			comment.save()
-	comments = Comment.objects.filter(quaker=quaker)
-	context = {
-		'quaker' : quaker,
-		'comments' : comments,
-		'form' : form,
-	}
+    quaker = Person.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                quaker=quaker
+            )
+            comment.save()
+    comments = Comment.objects.filter(quaker=quaker)
+    context = {
+        'quaker': quaker,
+        'comments': comments,
+        'form': form,
+    }
 
-	return render(request, 'quaker_detail.html', context)
+    return render(request, 'quaker_detail.html', context)
+
 
 def edit(request, pk, model, cls):
     quaker = get_object_or_404(Person, pk=pk)
@@ -64,48 +67,57 @@ def edit(request, pk, model, cls):
         return render(request, 'edit.html', {'form': form})
 
 
-
 def edit_quaker(request, pk):
     return edit(request, pk, Person, PersonForm)
 
 
-# class PersonListJson(BaseDatatableView):
-#     # the model you're going to show
-#     model = Person
-    
-#     # define columns that will be returned
-#     # they should be the fields of your model, and you may customize their displaying contents in render_column()
-#     # don't worry if your headers are not the same as your field names, you will define the headers in your template
-#     columns = ['firstname', 'lastname', 'middlename']
+class PersonJson(BaseDatatableView):
+    #     # the model you're going to show
+    model = Person
 
-#     # define column names that will be used in sorting 
-#     # order is important and should be same as order of columns displayed by datatables
-#     # for non sortable columns use empty value like ''
-#     order_columns = ['firstname', 'lastname', 'middlename']
+    #     # define columns that will be returned
+    #     # they should be the fields of your model, and you may customize their displaying contents in render_column()
+    #     # don't worry if your headers are not the same as your field names, you will define the headers in your template
+    columns = ['firstlastname', 'publication_name', "more_information"]
 
-#     # set max limit of records returned
-#     # this is used to protect your site if someone tries to attack your site and make it return huge amount of data
-#     max_display_length = 500
+    #     # define column names that will be used in sorting
+    #     # order is important and should be same as order of columns displayed by datatables
+    #     # for non sortable columns use empty value like ''
+    order_columns = ['firstlastname', 'publication_name', 'more_information']
 
-#     def render_column(self, row, column):
-#         # we want to render 'translation' as a custom column, because 'translation' is defined as a Textfield in Image model,
-#         # but here we only want to check the status of translating process.
-#         # so, if 'translation' is empty, i.e. no one enters any information in 'translation', we display 'waiting';
-#         # otherwise, we display 'processing'.
-#         if column == 'middlename':
-#             # escape HTML for security reasons
-#             return escape('{0} {1}'.format(row.firstname, row.lastname))
-#         else:
-#             return super(PersonListJson, self).render_column(row, column)
-#     def filter_queryset(self, qs):
-#         # use parameters passed in GET request to filter queryset
-        
-#         # here is a simple example
-#         search = self.request.GET.get('search[value]', None)
-#         if search:
-#             q = Q(firstname__icontains=search) | Q(lastname_icontains=search)
-#             qs = qs.filter(q)
-#         return qs
+    #     # set max limit of records returned
+    #     # this is used to protect your site if someone tries to attack your site and make it return huge amount of data
+    max_display_length = 500
+
+    def render_column(self, row, column):
+        # we want to render 'translation' as a custom column, because 'translation' is defined as a Textfield in Image model,
+        #         # but here we only want to check the status of translating process.
+        #         # so, if 'translation' is empty, i.e. no one enters any information in 'translation', we display 'waiting';
+        #         # otherwise, we display 'processing'.
+        if column == 'firstlastname':
+            #             # escape HTML for security reasons
+            return format_html('{}'.format(row.firstlastname))
+        if column == 'publication_name':
+            #             # escape HTML for security reasons
+            return format_html('{}'.format(row.publication_name))
+            if column == 'firstlastname':
+            # # escape HTML for security reasons
+            	return format_html('{}'.format(row.firstlastname))
+            if column == 'more_information':
+            # # escape HTML for security reasons
+            	return format_html("<a href='{}'>More information</a>").format(row.pk)
+            else:
+            	return super(PersonJson, self).render_column(row, column)
+
+    def filter_queryset(self, qs):
+        #         # use parameters passed in GET request to filter queryset
+
+        #         # here is a simple example
+        search = self.request.GET.get('search[value]', None)
+        if search:
+            q = Q(firstlastname__icontains=search) | Q(publication_name__icontains=search)
+            qs = qs.filter(q)
+        return qs
 
 # def contact(request):
 #     if request.method == 'POST':
